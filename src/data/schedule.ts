eb9df88d54784d223efb53a6abfc161b9b2e2ff7
liyan-nk <lnk.liyannk@gmail.com>
2026-09-07 15:07:00 +0000

    feat(data): populate Friday afternoon 1-hour periods P5 and P6

---
+++ src/data/schedule.ts
@@ -0,0 +1,4 @@
+import { PeriodDefinition } from '../types/schedule';
+import scheduleData from './schedule.json';
+
+export const BASE_SCHEDULE: PeriodDefinition[] = scheduleData as PeriodDefinition[];
